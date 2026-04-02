9ea6e33efa14f275541304558caf4965e38290c4
jullionetto-hub <jullionetto@hotmail.com>
2026-04-02 04:21:10 -0300

    corrige limite upload

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(cors({
     callback(null, origin || 'http://localhost:3000');
   }
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Sessão persistente com cookie correto para HTTP local
